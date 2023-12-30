6ad87ff819db932ba6a2661f249ba9bfccf7d091
w4yZ <phiriyakornman@gmail.com>
2023-12-30 13:29:33 +0700

    Form submission & Actions

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -6,6 +6,8 @@ const CreateEvent = () => {
 
   const userId = sessionClaims?.userId as string;
 
+  // Here 2:57:48
+
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
